ddde00116a8ffa51e0ee68244dd76ac8205369b9
Kinomagnum <kinomagnum@gmail.com>
2024-12-21 15:06:41 +0300

    House

--- src/main/java/housePrice/House.java
+++ src/main/java/housePrice/House.java
@@ -11,5 +11,59 @@ package housePrice;
    квадратного метра составляет 1500 единиц валюты. Метод должен возвращать общую стоимость дома на основе его площади.
 4. Реализуйте метод addRoom, который будет увеличивать количество комнат на заданное число. */
 public class House {
+   private String address;        // Адрес дома
+   private int numberOfRooms;     // Количество комнат
+   private double area;           // Площадь дома в квадратных метрах
 
+   // Конструктор для инициализации адреса, количества комнат и площади
+   public House(String address, int numberOfRooms, double area) {
+       this.address = address;
+       this.numberOfRooms = numberOfRooms;
+       this.area = area;
+   }
+
+   // Метод для получения адреса
+   public String getAddress() {
+       return address;
+   }
+
+   // Метод для установки адреса
+   public void setAddress(String address) {
+       this.address = address;
+   }
+
+   // Метод для получения количества комнат
+   public int getNumberOfRooms() {
+       return numberOfRooms;
+   }
+
+   // Метод для установки количества комнат
+   public void setNumberOfRooms(int numberOfRooms) {
+       this.numberOfRooms = numberOfRooms;
+   }
+
+   // Метод для получения площади
+   public double getArea() {
+       return area;
+   }
+
+   // Метод для установки площади
+   public void setArea(double area) {
+       this.area = area;
+   }
+
+   // Метод для вычисления стоимости дома
+   public double calculatePrice() {
+       final double pricePerSquareMeter = 1500; // Стоимость одного квадратного метра
+       return area * pricePerSquareMeter;       // Общая стоимость дома
+   }
+
+   // Метод для добавления комнат
+   public void addRoom(int roomsToAdd) {
+       if (roomsToAdd > 0) {
+           this.numberOfRooms += roomsToAdd; // Увеличиваем количество комнат
+       } else {
+           System.out.println("Количество добавляемых комнат должно быть положительным.");
+       }
+   }
 }
--- src/main/java/housePrice/MyFirstHouse.java
+++ src/main/java/housePrice/MyFirstHouse.java
@@ -2,7 +2,14 @@ package housePrice;
 
 public class MyFirstHouse {
     public static void main(String[] args) {
+        House myHouse = new House("123 Main St", 4, 2500);
         
+        System.out.println("Адрес: " + myHouse.getAddress());
+        System.out.println("Количество комнат: " + myHouse.getNumberOfRooms());
+        System.out.println("Площадь: " + myHouse.getArea() + " кв. м");
+        System.out.println("Стоимость дома: " + myHouse.calculatePrice() + " единиц валюты");
+        
+        myHouse.addRoom(2);
+        System.out.println("Новое количество комнат: " + myHouse.getNumberOfRooms());
     }
-
 }
